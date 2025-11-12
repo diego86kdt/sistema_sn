cf573e7f5ae66dfb021bf867af069fa1ecbd0e88
diego86kdt <diego.soares.info@gmail.com>
2025-11-12 20:21:16 -0300

    old

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario'])) header('Location: login.php');
-if ($_SESSION['perfil'] === 'administrador') header('Location: admin/index.php');
-else header('Location: fiscal/index.php');
-exit;
-?>
